1a1f461733d50c5d86b69ca80880fcae1261fc01
Eric Liu <eric@nomoss.co>
2025-07-21 14:15:33 +1000

    chore: add web analytics

--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -1,1 +1,19 @@
+import posthog from 'posthog-js'
+import { browser } from '$app/environment';
+
+export const load = async () => {
+  if (browser) {
+    posthog.init(
+      'phc_GdtfK5fSw0MieWN11HmniJtEqWjLHJ1l6cW72oTDVwc',
+      {
+        api_host: 'https://us.i.posthog.com',
+        defaults: '2025-05-24',
+        person_profiles: 'always',
+      }
+    )
+  }
+
+  return
+};
+
 export const prerender = true;
