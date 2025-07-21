c433693077ccce825633c8e6c9623b49a3b53d7b
Eric Liu <eric@nomoss.co>
2025-07-21 13:51:22 +1000

    chore: remove tailwind/typography and resolve lint errors

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,7 +6,7 @@
 
 <svelte:head>
 	<link rel="preconnect" href="https://fonts.googleapis.com" />
-	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous" />
 	<link
 		href="https://fonts.googleapis.com/css2?family=Oswald:wght@500&display=swap"
 		rel="stylesheet"
